6ce4840161e64898b8fd198dd7f64642f21c9822
Gilvan Neto <gilvanbrcneto@gmail.com>
2023-03-10 21:47:51 -0300

    create photo file error
    This commit modifies the system to allow only the upload of image files in PNG and JPG formats, restricting the upload of other file types.

--- src/routes/fotoRoutes.js
+++ src/routes/fotoRoutes.js
@@ -1,14 +1,10 @@
 import { Router } from 'express';
 // eslint-disable-next-line import/no-extraneous-dependencies
-import multer from 'multer';
 
 import fotoController from '../controllers/FotoController';
-import multerConfig from '../config/multer';
-
-const upload = multer(multerConfig);
 
 const router = new Router();
 
-router.post('/', upload.single('foto'), fotoController.store);
+router.post('/', fotoController.store);
 
 export default router;
